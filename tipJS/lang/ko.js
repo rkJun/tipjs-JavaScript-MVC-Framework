ddc8883e07c821f128022f0844c9335f45a92c39
SeungHyun PAEK <pekuid@gmail.com>
2013-01-11 08:04:52 +0900

    언어셋 파일 추가
    언어셋 파일 추가

---
+++ tipJS/lang/ko.js
@@ -0,0 +1,14 @@
+/*
+ * tipJS - Javascript MVC Framework ver.1.29
+ * 
+ * Copyright 2012.07 SeungHyun PAEK
+ * Dual licensed under the MIT or GPL Version 2 licenses.
+ * HomePage: http://www.tipjs.com
+ * Contact: http://www.tipjs.com/contact
+ */
+
+tipJS.localSet({
+	"Save":"저장",
+	"Load":"불러오기"
+});
+
