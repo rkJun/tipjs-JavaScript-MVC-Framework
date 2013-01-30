bcbcfdda411a812e4db122980a50ff73d2d72d8c
SeungHyun PAEK <pekuid@gmail.com>
2013-01-30 17:11:26 +0900

    version 1.30a Released
    - 버그수정

--- tipJS/tipJS-MVC-1.30a-dev.js
+++ tipJS/tipJS-MVC-1.30a-dev.js
@@ -1264,7 +1264,7 @@ tipJS.ver = tipJS.version = "1.30a";
 
 	for (var i = _scripts.length; i--;) {
 		_scriptSrc = _scripts[i].src;
-		_match = _scriptSrc.match(/tipJS-MVC-dev\.js$/);
+		_match = _scriptSrc.match(/tipJS-MVC-1\.30a-dev\.js$/);
 		if (_match) {
 			_filepath = _scriptSrc.substring(0, _scriptSrc.length - _match[0].length);
 			break;
