005f43243c704d3c6cd8fac6677bd5b4107f4796
SeungHyun PAEK <pekuid@gmail.com>
2013-05-05 21:08:09 +0900

    Update tipJS.config.js

--- tipJS.config.js
+++ tipJS.config.js
@@ -9,51 +9,37 @@
 
 
 // configuring tipJS
-tipJS.config(function() {
-	
-	var _ROOT = "/tipJS-dev";
-	
-	var config = {
-		noCache:true,
-		noCacheVersion:"1.000",
-		noCacheParam:"noCacheVersion",
-		noCacheAuto:false,
-		charSet:"utf-8",
-		developmentHostList:[
-			'localhost'
-			,'127.0.0.1'
-			,'tipjs.com'
-		],
-		commonLib:[
-			_ROOT+"/lib/jquery-ui-1.8.21/jquery-1.7.2.min.js?ver=1212"
-			//,"/tipJS-dev/lib/extjs-4.0.2a/ext-all-debug.js"
-		],
-		commonModel:[
-			_ROOT+"/common/models/CommonModel.js",
-			_ROOT+"/tutorial/common/commonParent.js",
-			_ROOT+"/tutorial/common/commonParent2.js",
-			_ROOT+"/tutorial/common/commonParent3.js"
-		],
-		commonView:[
-			_ROOT+"/common/views/CommonView.js",
-			_ROOT+"/tutorial/common/commonViewParent.js",
-			_ROOT+"/tutorial/common/commonViewParent2.js"
-		],
-		applicationPath:{
-			test : _ROOT+'/examples/test'
-			,test1 : _ROOT+'/test1'
-			
-			,Controller : _ROOT+'/tutorial/Controller'
-			,Model : _ROOT+'/tutorial/Model'
-			,ModelExtend : _ROOT+'/tutorial/ModelExtend'
-			,ModelSync : _ROOT+'/tutorial/ModelSync'
-			,ModelVO : _ROOT+'/tutorial/ModelVO'
-			,View : _ROOT+'/tutorial/View'
-			,ViewExtend : _ROOT+'/tutorial/ViewExtend'
-		}
-	};
-	
-	return config;
-	
-}());
+tipJS.config({
+	commonLib:[
+		"http://tipjs.com/tipJS/jquery-ui-1.8.21/jquery-1.7.2.min.js"
+		,"http://tipjs.com/tipJS/extjs-4.1.1/ext-all.js"
+	],
+	commonModel:[
+		"/tipJS/examples/common/models/CommonModel.js",
+		"/tipJS/tutorials/common/commonParent.js",
+		"/tipJS/tutorials/common/commonParent2.js",
+		"/tipJS/tutorials/common/commonParent3.js"
+	],
+	commonView:[
+		"/tipJS/examples/common/views/CommonView.js",
+		"/tipJS/tutorials/common/commonViewParent.js",
+		"/tipJS/tutorials/common/commonViewParent2.js"
+	],
+	applicationPath:{
+		geolocation : '/tipJS/examples/geolocation',
+		FileAPI : '/tipJS/examples/FileAPI',
+		helloWorld : '/tipJS/examples/helloWorld',
+		helloWorldTpl : '/tipJS/examples/helloWorldTpl'
+		,withExtJS : '/tipJS/examples/withExtJS'
+		,todoMVC : '/tipJS/examples/todoMVC'
+		
+		,Controller : '/tipJS/tutorials/Controller'
+		,Model : '/tipJS/tutorials/Model'
+		,ModelExtend : '/tipJS/tutorials/ModelExtend'
+		,ModelSync : '/tipJS/tutorials/ModelSync'
+		,ModelVO : '/tipJS/tutorials/ModelVO'
+		,View : '/tipJS/tutorials/View'
+		,ViewExtend : '/tipJS/tutorials/ViewExtend'
+	}
+});
 
